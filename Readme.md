67a59642bb367f71f77292ebd68bd30b367ee1a9
kellychin79 <kellychin79@gmail.com>
2022-11-21 00:26:21 -0500

    clean the data

--- Readme.md
+++ Readme.md
@@ -1,1 +1,17 @@
-Repo for analyzing XYZ
+Repo for analyzing population and meat consumption
+- The problem you decided to investigate
+	- How much of the US population is vegetarian? Has it increased or remained? (KL: Increase)
+	- Has the increase rate of meat consumption in the US surpass the growth rate of US population? (KL: Yes) By type?
+	- For the increasing meat consumption, have more livestock been slaughtered or only more weights been processed? (KL: by 1990 more weights after 2000 more livestock) By type?
+- How you are going to analyze data to answer that question
+	- Combine the population data and Livestock and Meat Domestic Data from US Department of Agriculture 
+- The results of your analysis, including how your visualizations are relevant and useful
+- Suggestions for future research (including improvements to what you did)
+	- Predict the C02 emissions from the numbers of livestock
+
+
+Terms of Livestock Slaughter
+https://beef2live.com/story-glossary-terms-livestock-slaughter-85-105350#:~:text=Average%20Live%20Weight%3A%20The%20weight,excludes%20animals%20slaughtered%20on%20farms.
+- Average Live Weight: The weight of the whole animal, before slaughter. Excludes post-mortem condemnations.
+- Commercial Production: Includes slaughter and meat production in federally inspected and other plants, but excludes animals slaughtered on farms. 
+- Dressed Weight: The weight of a chilled animal carcass. Beef with kidney knob in; veal with hide off; lamb and mutton with pluck out; pork with leaf fat and kidneys out, jowls on and head off.
--- population_meat_analysis.py
+++ population_meat_analysis.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[85]:
+# In[165]:
 
 
 import pandas as pd
 import numpy as np
 import re
+from datetime import datetime
 
 
-# In[7]:
+# In[166]:
 
 
-xlsx = pd.ExcelFile('meat_statistics.xlsx')
+xlsx = pd.ExcelFile('data/meat_statistics.xlsx')
 
 
 # The original excel file was designed for human readable, including merged cells for the first category (a.k.a Commerical vs. Federally Inspected below) and then individual cells for the secondary category (a.k.a row 0). 
 
-# In[80]:
+# In[167]:
 
 
 raw_data = pd.read_excel(xlsx, sheet_name = 'RedMeatPoultry_Prod-Full', header = 1)
@@ -26,7 +27,7 @@ raw_data.head()
 
 # There are two types - Commerical vs. Federally Inspected. Their numbers are pretty close. I decided to use the numbers under Federally Inspected because it contains more information in terms of meat types. 
 
-# In[81]:
+# In[168]:
 
 
 idx = list(raw_data.columns).index('Federally inspected')
@@ -36,7 +37,7 @@ for i in range(len(raw_data.columns)):
         idxs.append(i)
 
 
-# In[82]:
+# In[169]:
 
 
 raw_data = raw_data.iloc[:, idxs]
@@ -45,13 +46,13 @@ raw_data.head()
 
 # Replace the current header with the first row (a.k.a the secondary categories) and remove the empty column
 
-# In[83]:
+# In[170]:
 
 
 new_header = raw_data.iloc[0, :-1]
-meat_prod = raw_data.iloc[1:, :-1]
-meat_prod.columns = new_header
-meat_prod.head()
+raw_data = raw_data.iloc[1:, :-1]
+raw_data.columns = new_header
+raw_data.head()
 
 
 # Transform the header by removing space and notation, explained below
@@ -62,15 +63,60 @@ meat_prod.head()
 # - 5/ Ready-to-cook.																
 # - 6/ Includes geese, guineas, ostriches, emus, rheas, squab, and other poultry.																
 
-# In[84]:
+# In[171]:
 
 
-current_header = meat_prod.columns[1:] 
+current_header = raw_data.columns[1:] 
 transformed_header = ['Month']
 for i in current_header:
+    word = re.search('\D+', i).group().strip().replace(' ', '_')
     transformed_header.append(word.lower())
-meat_prod.columns = transformed_header
+raw_data.columns = transformed_header
+raw_data.head()
+
+
+# Remove the rows containing additional information
+
+# In[172]:
+
+
+raw_data.tail(10)
+
+
+# In[175]:
+
+
+trim_data = raw_data[~raw_data['Month'].str.contains('/ |Source|Date run')]
+trim_data.tail()
+
+
+# Keep only the monthly data and then convert to datetime data type
+
+# In[185]:
+
+
+trim_data.head()
+
+
+# In[184]:
+
+
+meat_prod = trim_data.copy()
+meat_prod = meat_prod[meat_prod['Month'].str.contains(r'\w{3}-\d{4}', regex=True)]
 meat_prod.head()
+
+
+# In[188]:
+
+
+meat_prod['Month'] = meat_prod['Month'].apply(lambda x: datetime.strptime(x, '%b-%Y'))
+meat_prod.head()
+
+
+# In[189]:
+
+
+meat_prod.info()
 
 
 # In[ ]:
